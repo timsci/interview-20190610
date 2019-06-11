5aa165506db9f562aa78f43c07fa7626db615c70
timsci <timscidev@gmail.com>
2019-06-10 17:41:30 -0700

    Added edit view and controller route

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -56,41 +56,60 @@ class ContactController extends Controller
         $contact->fill($validatedData);
         $contact->save();
 
-        return \Redirect::route("contacts.show", [$contact->id]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+        return \Redirect::route("contacts.edit", [$contact]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param int $id
+     * @param Contact $contact
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Contact $contact)
     {
-        //
+        $states = require app_path("states.php");
+
+        $birthday_year = "";
+        $birthday_month = "";
+        $birthday_day = "";
+        if ($contact->birthday) {
+            list($birthday_year, $birthday_month, $birthday_day) = explode("-", $contact->birthday);
+        }
+
+        return view("pages.edit", compact(
+            "contact",
+            "states",
+            "birthday_year",
+            "birthday_month",
+            "birthday_day"
+        ));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param \Illuminate\Http\Request $request
-     * @param int $id
+     * @param Contact $contact
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Contact $contact)
     {
-        //
+        $validatedData = $request->validate([
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'phone' => 'max:255',
+            'birthday' => 'nullable|date',
+            'address' => 'max:255',
+            'city' => 'max:255',
+            'state' => [new StateRule()],
+            'zipcode' => 'max:255'
+        ]);
+
+        $contact->fill($validatedData);
+        $contact->save();
+
+        return \Redirect::route("contacts.edit", [$contact]);
     }
 
     /**
